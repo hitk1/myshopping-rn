b6878235ac1eb2d27be697cf0d00f8fc1b01a2f3
hitk1 <luispaulo.degini@gmail.com>
2022-06-25 17:51:29 -0300

    feat: leitura dos dados no firestore

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlatList } from 'react-native';
+import firestore from '@react-native-firebase/firestore'
 
 import { styles } from './styles';
 import { Product, ProductProps } from '../Product';
@@ -7,7 +8,41 @@ import { Product, ProductProps } from '../Product';
 import { shoppingListExample } from '../../utils/shopping.list.data';
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  //Busca em realtime
+  // useEffect(() => {
+  //   const subscription = firestore()
+  //     .collection('products')
+  //     .onSnapshot(query => {
+  //       const data = query.docs.map(doc => ({
+  //         id: doc.id,
+  //         ...doc.data()
+  //       })) as ProductProps[]
+
+  //       setProducts(data)
+  //     })
+
+  //   return () => subscription()
+  // })
+
+  //busca de todos os documentos uma unica vez
+  useEffect(() => {
+    firestore()
+      .collection('products')   //Caso seja necessário buscar somente um documento, acrescentar um .doc depois de 'collection', passando o ID como parametro
+      .get()
+      .then(response => {
+        const data = response.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        })) as ProductProps[]
+
+        setProducts(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
 
   return (
     <FlatList
